4a0848962e7058b5d9a173d9928bd515f76ae927
Maximilian Mackson <48737600+TheMilianMan@users.noreply.github.com>
2019-11-18 02:03:24 -0500

    Added basic proof-of-concept code
    Added code to query Chrome APIs for currently open tabs, cookies, and top-level bookmarks (i.e., those not in folders).

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,34 @@
-import { helloWorld } from './hello-world.js';
+// Log open tabs in current window to console
+const logTabs = (tabArray) => {
+    tabArray.forEach(tab => {
+        // console.log(tab.url);
+    });
 
-helloWorld();
+    console.log(`${tabArray.length} tabs total`);
+}
+
+chrome.tabs.query({ currentWindow: true }, (tabs) => logTabs(tabs));
+
+// Log cookies in current execution session to window
+const logCookies = (cookieArray) => {
+    cookieArray.forEach(cookie => {
+        // console.log(cookie.domain);
+    });
+
+    console.log(`${cookieArray.length} cookies total`);
+}
+
+chrome.cookies.getAll({}, (cookies) => logCookies(cookies));
+
+// Log bookmarks in main subtree to console
+const logBookmarks = (bookmarkNodeArray) => {
+    bookmarkNodeArray.forEach(bookmarkNode => {
+        bookmarkNode.children.forEach(bookmarkSubNode => {
+            console.log(bookmarkSubNode.title);
+        });
+
+        console.log(`${bookmarkNode.children.length} bookmarks total`);
+    });
+}
+
+chrome.bookmarks.getSubTree('1', (bookmarks) => logBookmarks(bookmarks));
